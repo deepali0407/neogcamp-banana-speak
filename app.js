d48afcf01691d62846b06ca8d2bc7fd2517b901e
deepali0407 <deepalirajvanshi04@gmail.com>
2021-08-28 16:03:22 +0530

    docs: updated .js file
    used functions constructURL ,errorHandler and clickHandler. app working now.

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 var btnTranslate = document.querySelector("#btn-Translate")
+var txtInput = document.querySelector("#txt-input");
+var txtOutput = document.querySelector("#txt-output");
+
+var serverURL= "	https://api.funtranslations.com/translate/minion.json"
+
+function constructURL(text){
+   return serverURL + "?" + "text=" + text;
+}
+
+
+function errorHandler(){
+    console.log("error occured",error);
+    alert("Something wrong with server. Try Again after sometime");
+}
 
 function clickHandler(){
-    console.log("clicked");
-}
+
+    var inputText = txtInput.value;
+
+     fetch(constructURL(inputText))
+     .then(response => response.json())
+     .then(json => {
+         var translatedText = json.contents.translated
+         txtOutput.innerText= translatedText
+        })
+    }
 
 btnTranslate.addEventListener("click", clickHandler);
